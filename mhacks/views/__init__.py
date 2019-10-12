178112d0f8caf5ed93a1b0828110753933d995a5
andlu <andlu@umich.edu>
2019-10-12 13:25:09 -0400

    Deleted some unnecessary files

--- mhacks/views/__init__.py
+++ mhacks/views/__init__.py
@@ -6,10 +6,4 @@ from mhacks.views.create_user import create_user
 from mhacks.views.edit import edit
 from mhacks.views.delete import delete
 from mhacks.views.password import password
-from mhacks.views.static import show_image
-from mhacks.views.explore import show_explore
-from mhacks.views.follower import show_follower
-from mhacks.views.following import show_following
-from mhacks.views.user import show_user
-from mhacks.views.post import show_post
 from mhacks.views.home import home
